d1a9470243e547d643cc1b68993d4b4c25b1a256
ekong <sesaclnk5512@gmail.com>
2026-07-02 14:45:34 +0900

    main game 1

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from game import Game
+from monster import Mushroom
 
 if __name__ == "__main__":
+    # 1. Game 객체 생성
     game = Game()
+    
+    # 2. game.py를 수정하지 않고 Mushroom을 사용하도록 self.mob 오버라이딩
+    game.mob = Mushroom("주황버섯", hp=70, attack_power=12, defense=2)
+    game.mob.max_hp = 70
+    
+    # 3. 게임 시작
     game.start()
